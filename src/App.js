72889c49f4310008be0b2a4a0606a351b4b8eae2
baileyconradt <50774447+baileyconradt@users.noreply.github.com>
2024-11-04 22:06:47 -0600

    initial commit, connected to the ravelry API via fetch using basic auth

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+var base64 = require('base-64');
 
 function App() {
+  const [data, setData] = useState({});
+  let patterns = <></>
+
+  useEffect(() => {
+
+
+    let url = 'https://api.ravelry.com/patterns/search.json';
+    let username = process.env.REACT_APP_USERNAME;
+    let password = process.env.REACT_APP_PASSWORD;
+    let headers = new Headers();
+    headers.set('Authorization', 'Basic ' + base64.encode(username + ":" + password));
+
+    fetch(url, {
+      method: 'GET',
+      headers: headers,
+    }).then((res) => res.json())
+      .then((json) => {
+        console.log(json)
+        setData(json);
+      });
+
+  }, []);
+
+  if (data && data.patterns) {
+    console.dir(data.patterns)
+    patterns = data.patterns.map((pattern) =>
+      <>
+        <div className="card">
+          <p>{pattern.id}</p>
+          <p>{pattern.name}</p>
+        </div>
+      </>
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {patterns}
+
+        {/* <>{JSON.stringify(data)}</> */}
       </header>
     </div>
   );
